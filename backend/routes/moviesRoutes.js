5b4b5371276b21a60ab7fac2d0f17fda3005ac09
angycervantes <angelica.cervantes.e@gmail.com>
2023-07-26 17:54:52 -0600

    prueba de cambios al dev

--- backend/routes/moviesRoutes.js
+++ backend/routes/moviesRoutes.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const { getMovies, createMovies, updateMovies, deleteMovies } = require('../controllers/movieControllers')
 
-//router.route('/').get(getMovies).post(createMovies)
+router.route('/').get(getMovies).post(createMovies)
 router.get('/', getMovies)
 router.post('/', createMovies)
 
 router.route('/:id').delete(deleteMovies).put(updateMovies)
-//router.put('/:id', updateMovies)
+router.put('/:id', updateMovies)
 //router.delete('/:id', deleteMovies)
 
 
